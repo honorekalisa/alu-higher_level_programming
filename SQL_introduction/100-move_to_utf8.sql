10712184c664411f73c06db16ca821871842172c
honorekalisa <h.kalisa@alustudent.com>
2023-03-31 11:30:45 +0200

    advanced

--- SQL_introduction/100-move_to_utf8.sql
+++ SQL_introduction/100-move_to_utf8.sql
@@ -1,9 +1,6 @@
 -- converts hbtcn database to utf8,collate utf8_unicode_ci
-ALTER DATABASE hbtn_0c_0 
-CHARACTER SET utf8 
-COLLATE utf8_unicode_ci;
+ALTER DATABASE hbtn_0c_0 CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
 
-ALTER TABLE first_table 
-IN DATABASE hbtn_0c_0 
-CONVERT TO CHARACTER SET utf8 
-COLLATE utf8_unicode_ci;
+ALTER TABLE first_table CONVERT TO CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
+
+ALTER TABLE first_table MODIFY COLUMN name VARCHAR(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
