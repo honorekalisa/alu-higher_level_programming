830361122e49c552864926a86f5daa8be5461d21
honorekalisa <h.kalisa@alustudent.com>
2023-06-07 17:33:45 +0200

    concat.js

--- javascript-warm_up/4-concat.js
+++ javascript-warm_up/4-concat.js
@@ -1,9 +1,4 @@
 #!/usr/bin/node
-const argument1 = process.argv[2];
-const argument2 = process.argv[3];
-
-if (argument1 === undefined || argument2 === undefined) {
-  console.log('HBTN is undefined');
-} else {
-  console.log(`${argument1} is ${argument2}`);
-}
+const firstArgv = process.argv[2];
+const secondArgv = process.argv[3];
+console.log(firstArgv + ' is ' + secondArgv);
