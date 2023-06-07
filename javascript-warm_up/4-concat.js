fde12982adf3c346f169035e441b278e21cca36f
honorekalisa <h.kalisa@alustudent.com>
2023-06-07 17:32:06 +0200

    bug fix

--- javascript-warm_up/4-concat.js
+++ javascript-warm_up/4-concat.js
@@ -3,7 +3,7 @@ const argument1 = process.argv[2];
 const argument2 = process.argv[3];
 
 if (argument1 === undefined || argument2 === undefined) {
-  console.log('undefined is undefined');
+  console.log('HBTN is undefined');
 } else {
   console.log(`${argument1} is ${argument2}`);
 }
