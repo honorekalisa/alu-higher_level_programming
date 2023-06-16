9cc23d76550045099037b7d6a4fb39453ddfaf39
honorekalisa <h.kalisa@alustudent.com>
2023-06-16 11:57:04 +0200

    task 2

---
+++ javascript-warm_up/101-call_me_moby.js
@@ -0,0 +1,12 @@
+#!/usr/bin/node
+const executeNTimes = function (x, theFunction) {
+  for (let i = 0; i < x; i++) {
+    theFunction();
+  }
+};
+
+function callMeMoby() {
+  console.log("C is fun");
+}
+
+executeNTimes(100, callMeMoby());
